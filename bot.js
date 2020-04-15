d704b7309f793ba90e735449ac87b746e24eee66
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-04-15 17:50:18 +0100

    genio bot setup

---
+++ bot.js
@@ -0,0 +1,57 @@
+import express from 'express';
+const bot = express();
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+
+bot.use(express.json());
+const writeFile = fs.createWriteStream(path.join(__dirname, "/server.log"), {
+  flags: "a",
+  encoding: "utf8",
+});
+
+const format = ":method\t:url\t:status\t:response-time";
+
+bot.use(
+  morgan(format, {
+    stream: {
+      write(message) {
+        const finalIndex = message.length - 1;
+        const lastTabIndex = message.lastIndexOf("\t");
+        const str = message.substring(lastTabIndex + 1, finalIndex);
+        let time = Math.ceil(parseFloat(str));
+        if (time < 10) {
+          time = `0${time.toString()}`;
+        } else {
+          time = time.toString();
+        }
+        const msg = `${message.substring(0, lastTabIndex + 1)}${time}ms\n`;
+        writeFile.write(msg);
+      },
+    },
+  })
+);
+
+bot.get('/', (req, res)=> {
+    res.status(200).json({
+        message:'I am Geniobot add me on +2348121694384'
+    })
+})
+
+bot.post('/', (req, res)=> {
+    let data = req.body;
+    console.log(data);
+    if(data.messages && data.messages[0].body.length > 0) {
+
+    }
+    res.end();
+})
+
+process.env.NODE_ENV === "development"
+  ? bot.listen(4000, () =>
+      console.log(`Geniobot working on port ${4000}`)
+    )
+  : bot.listen(process.env.PORT, process.env.IP, () =>
+      console.log(`Geniobot working on port ${process.env.PORT}`)
+    );
