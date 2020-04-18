4576cbad958550d2cbd76e4efaf29c47b13d1dd8
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-04-18 14:55:08 +0100

    root menu iss

--- bot.js
+++ bot.js
@@ -83,8 +83,7 @@ bot.post('/', async(req, res)=> {
             \n*I am Genio*, and these are the things for you:
             \n*1. Genio-Status* \nUsage: You send Genio-Status \nWith this I can help you upload your content to my status, and keep a Record of your shared status. 
             \n*2. Genio-Covid19* \nSend you information about Coronavirus\nSend me: Genio-Covid <CountryName> to get Covid-Data about the country
-            \n*3. Genio-Util* \nSend me this to send you procedure for getting your payment for utilities all sorted here on whatsapp.
-            \n*4. Genio-Share* - Share your messages to groups.\nAdd me to two groups and I can help you share messages from one group to the other you don't need a telegram version of your group again.
+            \n*3. Genio-Share* - Share your messages to groups.\nAdd me to two groups and I can help you share messages from one group to the other you don't need a telegram version of your group again.
             \nYou can send the text or the number to enter the mode of operation you want.
             \n\n🕵️‍♀️ *I am Genio*, and I am here to serve you.🏋️‍♀️\n`,
             }
@@ -245,7 +244,9 @@ bot.post('/', async(req, res)=> {
               `
           }
         )
-        .then(updated=>console.log(updated.data))
+        .then(updated=>{
+          let
+        })
         .catch(err=> console.log(err.message))
           }
 
